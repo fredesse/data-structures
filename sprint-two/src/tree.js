a531af8a12f47e3c781a2a08b9563eae725f6784
fredesse <eleegia@gmail.com>
2017-07-08 16:45:33 -0700

    Pomandeered tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,37 +1,36 @@
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
   _.extend(newTree, treeMethods);
-  newTree.children = [];  // fix me
+  newTree.children = [];
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
   this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target, node) {
+treeMethods.contains = function (target, node) {
   node = node || this;
   //check if parent has target
-  if(node.value === target){
+  if (node.value === target) {
     //if yes then return true
     return true;
   }
   //if not then go over the children
-  for(var i = 0; i < node.children.length; i++){
+  for (var i = 0; i < node.children.length; i++) {
     //check if child has target
-    if(this.contains(target, node.children[i])){
+    if (this.contains(target, node.children[i])) {
       return true;
     }
   }
-
   return false;
-  };
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
