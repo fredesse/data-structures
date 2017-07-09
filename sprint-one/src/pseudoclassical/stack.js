adb181d83b48c0bcf71fe775736fde1cf65e7887
fredesse <eleegia@gmail.com>
2017-07-08 17:03:01 -0700

    Pomandeered pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -6,15 +6,15 @@ var Stack = function() {
 
 Stack.prototype.push = function(value){
   this.storage[this.size()] = value;
-}
+};
 
 Stack.prototype.pop = function(){
   var temp = this.storage[this.size() - 1];
   delete this.storage[this.size() - 1];
   return temp;
-}
+};
 
-Stack.prototype.size = function(){
+Stack.prototype.size = function() {
   return Object.keys(this.storage).length;
-}
+};
 
