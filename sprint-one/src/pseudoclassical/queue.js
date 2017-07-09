f03f3a3cf224d8135434b5041bde4a864c4d8157
fredesse <eleegia@gmail.com>
2017-07-08 17:05:04 -0700

    Pomandeered pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -6,18 +6,18 @@ var Queue = function() {
 
 Queue.prototype.enqueue = function(value) {
   this.storage[this.size()] = value;
-}
+};
 
 Queue.prototype.dequeue = function() {
   var temp = this.storage[0];
   delete this.storage[0];
-  for(var key in this.storage){
+  for (var key in this.storage) {
     this.storage[key - 1] = this.storage[key];
   }
   delete this.storage[this.size() - 1];
   return temp;
-}
+};
 
 Queue.prototype.size = function() {
   return Object.keys(this.storage).length;
-}
+};
