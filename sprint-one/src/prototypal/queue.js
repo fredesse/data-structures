6bcef92504d4789128445ce3265e13af611912ad
fredesse <eleegia@gmail.com>
2017-07-08 17:01:41 -0700

    Pomandeered prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -10,18 +10,18 @@ var queueMethods = {};
 
 queueMethods.enqueue = function(value) {
   this.storage[this.size()] = value;
-}
+};
 
 queueMethods.dequeue = function() {
   var temp = this.storage[0];
   delete this.storage[0];
-  for(var key in this.storage) {
-    this.storage[key-1] = this.storage[key];
+  for (var key in this.storage) {
+    this.storage[key - 1] = this.storage[key];
   }
-  delete this.storage[this.size()-1];
+  delete this.storage[this.size() - 1];
   return temp;
-}
+};
 
 queueMethods.size = function() {
   return Object.keys(this.storage).length;
-}
+};
